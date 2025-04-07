2de23632695836dd6b327c8d057d7bca7c1c3823
usuba.yusuke <usuba.yusuke@F-00490-Mac.local>
2025-04-07 19:24:15 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
